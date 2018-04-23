858599ebcfa58ba3ae1d8cfd22c4b5b8eeddf1f2
Mohammed Boumnadel <boumnadel.med@gmail.com>
2018-04-22 23:53:06 -0400

    Thymeleaf integration

--- src/main/java/com/med/firstapp/controller/OrderController.java
+++ src/main/java/com/med/firstapp/controller/OrderController.java
@@ -16,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,7 +48,7 @@ public class OrderController {
 	@Autowired
 	private ProductService productService;
 
-	//@InitBinder(value = "order")
+	@InitBinder(value = "order")
 	protected void initBinder(WebDataBinder binder) {
 
 	    ///binder.setConversionService(conversionService);
